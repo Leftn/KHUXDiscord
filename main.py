dfb76b7637fa8a7d9b04954917069d6409f5b722
g_ <14917082+Leftn@users.noreply.github.com>
2018-03-29 14:23:34 +1100

    Added new field to config, Moved the embed creation to it's own function

--- main.py
+++ main.py
@@ -17,18 +17,25 @@ config = json.load(open("config.json", "r"))
 @bot.command()
 async def get(*args):
     medal_data = get_medal(args)
-    embed = Embed(inline=True, colour=helpers.map_element_to_colour(medal_data.get("element")))
     if not medal_data.get("error"): # No errors here
-        embed.add_field(name=medal_data.get("name"), value=medal_data.get("notes"))
         url = "https://www.khuxbot.com"+medal_data.get("image_link")
-        embed.set_image(url=url)
-        embed.add_field(name=medal_data.get("element"), value=medal_data.get("direction"))
+        embed = create_embed(medal_data, url)
         await bot.say("", embed=embed)
     else:
         await bot.say("Could not find medal **{}**\nDid you mean:\n{}".format(" ".join(args), "\n".join(medal_data.get("error"))))
 
 def get_suggested_names(failed_name):
-    return difflib.get_close_matches(failed_name.lower(), names, cutoff=0.7)
+    return difflib.get_close_matches(failed_name.lower(), names, cutoff=config.get("medal_similarity_score"))
+
+def create_embed(medal_data, url):
+    embed = Embed(colour=helpers.map_element_to_colour(medal_data.get("element")))
+    embed.add_field(name=medal_data.get("name"), value=medal_data.get("notes"), inline=False)
+    emojis = {x.name: x for x in bot.get_all_emojis()}
+    embed.set_image(url=url)
+    embed.add_field(name=medal_data.get("element") + str(emojis.get(medal_data.get("element"))),
+                    value=medal_data.get("direction"), inline=False)
+    embed.add_field(name="", value="", inline=True)
+    return embed
 
 
 def get_medal(name):
@@ -44,6 +51,6 @@ def get_medal(name):
 
 
 if __name__ == "__main__":
-    print("Use the following url to connect the bot to your server")
+    print("Use the following url to connect the bot to your server:")
     print(oauth_url(config.get("client_id")))
     bot.run(config.get("token_secret"))
